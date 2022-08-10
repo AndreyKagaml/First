7e688ec55a2694a55dc606ad405577f743f47286
Andrey Kahamlyk <kagamlykas@gmail.com>
2022-08-11 00:59:17 +0300

    return last version

--- FirstFile.java
+++ FirstFile.java
@@ -1,12 +1,6 @@
 class Hello{
 	public static void main (String [] args){
-		for(int i = 0; i<5; i++){
-			System.out.println(i*3);
-		}
-		System.out.println("Hello world!");
-		List<Integer> list = new ArrayList<>();
-		list.add(8);
-		list.add(12);
 		
+		System.out.println("Hello world!");
 	}
 }
