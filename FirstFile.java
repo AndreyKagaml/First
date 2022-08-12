8f725f17cb1d5ab56b039112db5b62245afaff59
Andrey Kahamlyk <kagamlykas@gmail.com>
2022-08-12 11:43:39 +0300

    Add class Person

--- FirstFile.java
+++ FirstFile.java
@@ -6,5 +6,16 @@ class Hello{
 		list.add(10);
 		list.add(12);
 		list.add(524);
+		
+	}
+}
+
+class Person{
+	private String name;
+	private int id;
+	
+	public Person(int id, String name){
+		this.id = id;
+		this.name = name;
 	}
 }
