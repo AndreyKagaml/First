25aedb6abd5c0c920df420f69c0e2e702d29df0d
Andrey Kahamlyk <kagamlykas@gmail.com>
2022-08-12 10:02:10 +0300

    add list

--- FirstFile.java
+++ FirstFile.java
@@ -2,5 +2,9 @@ class Hello{
 	public static void main (String [] args){
 		
 		System.out.println("Hello world!");
+		List <Integer> list = ArrayList<>();
+		list.add(10);
+		list.add(12);
+		list.add(524);
 	}
 }
