7dc9e43ca61e614c4976c5ba78a39ff3023c6cda
Andrey Kahamlyk <kagamlykas@gmail.com>
2022-08-16 00:32:54 +0300

    Add getter in Work class

--- Work.java
+++ Work.java
@@ -2,6 +2,10 @@ class Work{
 private int id;
 private String description;
 
+public int getId(){
+	return this.id;
+}
+
 public Work(int id, String description){
 this.id = id;
 this.description = description;
